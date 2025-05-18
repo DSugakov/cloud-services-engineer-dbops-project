5f5a6c6e3199cd8df52cb2ebafb2e6d87c1c3edb
Sugakov Denis <Sugakovdv@yandex.ru>
2025-05-18 16:16:33 +0300

    fix

--- migrations/V001__create_tables.sql
+++ migrations/V001__create_tables.sql
@@ -1,33 +1,29 @@
-CREATE TABLE IF NOT EXISTS product (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE product (
+    id BIGINT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     picture_url VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS product_info (
+CREATE TABLE product_info (
     product_id BIGINT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    price DOUBLE PRECISION,
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    price DOUBLE PRECISION
 );
 
-CREATE TABLE IF NOT EXISTS orders (
-    id SERIAL PRIMARY KEY,
+CREATE TABLE orders (
+    id BIGINT PRIMARY KEY,
     status VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS orders_date (
+CREATE TABLE orders_date (
     order_id BIGINT PRIMARY KEY,
     status VARCHAR(255),
-    date_created DATE DEFAULT CURRENT_DATE,
-    FOREIGN KEY (order_id) REFERENCES orders(id)
+    date_created DATE
 );
 
-CREATE TABLE IF NOT EXISTS order_product (
+CREATE TABLE order_product (
     quantity INTEGER NOT NULL,
     order_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    PRIMARY KEY (order_id, product_id),
-    FOREIGN KEY (order_id) REFERENCES orders(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    PRIMARY KEY (order_id, product_id)
 );
