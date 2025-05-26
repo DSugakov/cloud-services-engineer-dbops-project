29ae29760f76d2e0735d4431be2d2ed1cc2d3a63
Sugakov Denis <Sugakovdv@yandex.ru>
2025-05-26 10:09:06 +0300

    fix

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -1,29 +1,31 @@
-ALTER TABLE product
-    ADD COLUMN price DOUBLE PRECISION;
+SELECT 1; -- Тестовый оператор
 
-UPDATE product p
-SET    price = pi.price
-FROM   product_info pi
-WHERE  p.id = pi.product_id;
+-- ALTER TABLE product
+--     ADD COLUMN price DOUBLE PRECISION;
 
-ALTER TABLE orders
-    ADD COLUMN date_created DATE;
+-- UPDATE product p
+-- SET    price = pi.price
+-- FROM   product_info pi
+-- WHERE  p.id = pi.product_id;
 
-UPDATE orders o
-SET    date_created = od.date_created
-FROM   orders_date od
-WHERE  o.id = od.order_id;
+-- ALTER TABLE orders
+--     ADD COLUMN date_created DATE;
 
-UPDATE orders o
-SET    status = COALESCE(o.status, od.status)
-FROM   orders_date od
-WHERE  o.id = od.order_id;
+-- UPDATE orders o
+-- SET    date_created = od.date_created
+-- FROM   orders_date od
+-- WHERE  o.id = od.order_id;
 
-ALTER TABLE order_product
-    ADD CONSTRAINT fk_order_product_product
-        FOREIGN KEY (product_id) REFERENCES product(id),
-    ADD CONSTRAINT fk_order_product_order
-        FOREIGN KEY (order_id) REFERENCES orders(id);
+-- UPDATE orders o
+-- SET    status = COALESCE(o.status, od.status)
+-- FROM   orders_date od
+-- WHERE  o.id = od.order_id;
 
-DROP TABLE product_info;
-DROP TABLE orders_date;
+-- ALTER TABLE order_product
+--     ADD CONSTRAINT fk_order_product_product
+--         FOREIGN KEY (product_id) REFERENCES product(id),
+--     ADD CONSTRAINT fk_order_product_order
+--         FOREIGN KEY (order_id) REFERENCES orders(id);
+
+-- DROP TABLE product_info;
+-- DROP TABLE orders_date;
