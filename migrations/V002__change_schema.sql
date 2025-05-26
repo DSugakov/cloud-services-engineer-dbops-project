3d40af1a768d9b756d7c1ae7fb72b8649a6c4999
Sugakov Denis <Sugakovdv@yandex.ru>
2025-05-26 14:13:13 +0300

    fix

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -9,36 +9,14 @@ ALTER TABLE product
 -- FROM   product_info pi
 -- WHERE  p.id = pi.product_id;
 
-UPDATE product p
-SET    price = pi.price
-FROM   product_info pi
-WHERE  p.id = pi.product_id;
-
 -- ALTER TABLE orders
 --     ADD COLUMN date_created DATE;
 
-ALTER TABLE orders
-    ADD COLUMN date_created DATE;
-
 -- UPDATE orders o
--- SET    date_created = od.date_created
+-- SET    date_created = od.date_created,
+--        status = COALESCE(o.status, od.status)
 -- FROM   orders_date od
 -- WHERE  o.id = od.order_id;
-
-UPDATE orders o
-SET    date_created = od.date_created
-FROM   orders_date od
-WHERE  o.id = od.order_id;
-
--- UPDATE orders o
--- SET    status = COALESCE(o.status, od.status)
--- FROM   orders_date od
--- WHERE  o.id = od.order_id;
-
-UPDATE orders o
-SET    status = COALESCE(o.status, od.status)
-FROM   orders_date od
-WHERE  o.id = od.order_id;
 
 -- ALTER TABLE order_product
 --     ADD CONSTRAINT fk_order_product_product
@@ -46,14 +24,5 @@ WHERE  o.id = od.order_id;
 --     ADD CONSTRAINT fk_order_product_order
 --         FOREIGN KEY (order_id) REFERENCES orders(id);
 
-ALTER TABLE order_product
-    ADD CONSTRAINT fk_order_product_product
-        FOREIGN KEY (product_id) REFERENCES product(id),
-    ADD CONSTRAINT fk_order_product_order
-        FOREIGN KEY (order_id) REFERENCES orders(id);
-
 -- DROP TABLE product_info;
 -- DROP TABLE orders_date;
-
-DROP TABLE product_info;
-DROP TABLE orders_date;
