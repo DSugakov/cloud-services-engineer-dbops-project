3029648940c5408e92cd4ae4d59331627dc4df22
Sugakov Denis <Sugakovdv@yandex.ru>
2025-05-26 14:43:23 +0300

    all fix

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -1,5 +1,10 @@
+-- V002__change_schema.sql
+-- Добавляем столбец price в таблицу product с типом NUMERIC для точности денежных значений
 ALTER TABLE product ADD COLUMN price NUMERIC(10, 2);
+
+-- Добавляем столбец date_created в таблицу orders
 ALTER TABLE orders ADD COLUMN date_created DATE DEFAULT current_date;
 
-DROP TABLE product_info;
-DROP TABLE orders_date;
+-- Удаляем таблицы product_info и orders_date, если они существуют
+DROP TABLE IF EXISTS product_info;
+DROP TABLE IF EXISTS orders_date;
